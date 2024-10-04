c0a17963254113fdb7f0e8641820b22354a37834
Mradroit611 <sanjayvermaa12121@gmail.com>
2024-10-04 20:08:50 +0530

    result page

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './results.component.html',
   styleUrl: './results.component.scss'
 })
 export class ResultsComponent {
 
+  totalAnimalsFed: number = 0;
+  totalBowlsFilled: number = 0;
+  locationsCovered: number = 0;
+  volunteerHours: number = 0;
+  storiesShared: number = 0;
+
+  constructor() {
+    // Initialize with any default values or fetch data from a service
+  }
 }
+ 
